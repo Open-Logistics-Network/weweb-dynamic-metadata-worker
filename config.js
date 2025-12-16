7fe7e86d1afc23cdd52848296b810e042b1a31a4
An Hoang <66811459+htahta103@users.noreply.github.com>
2025-12-16 15:14:38 +0700

    update

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
 export const config = {
-  domainSource: "https://www.openlogistics.network", // Your WeWeb app preview link
-  patterns: [
+    domainSource: "https://oln-mainapplication-repository.pages.dev",
+    // Your WeWeb app preview link
+    patterns: [
+      // {
+      //   pattern: "/event/[^/]+",
+      //   metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:8wD10mRd/event/{id}/meta"
+      // },
       {
-          pattern: "/locations/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:8wD10mRd/locations/{id}"
-      },
-      {
-          pattern: "/location/metadata/[^/]+",
-          metaDataEndpoint: "https://xxgx-bhd0-k4hs.f2.xano.io/api:JyhvLhrj/location/metadata/{internal_location_id}"
+        pattern: "/locations/[^/]+",
+        metaDataEndpoint: "https://xxgx-bhd0-k4hs.f2.xano.io/api:JyhvLhrj/location/metadata/{id}"
       }
       // Add more patterns and their metadata endpoints as needed
-  ]
-};
+    ]
+  };
