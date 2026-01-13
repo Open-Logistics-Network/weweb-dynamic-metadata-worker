c2b174382dab8218be4e7adb86f023b3b4eb9e6b
An Hoang <66811459+htahta103@users.noreply.github.com>
2026-01-13 15:55:57 +0700

    Update config.js (#6)

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ export const config = {
       //   metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:8wD10mRd/event/{id}/meta"
       // },
       {
-        pattern: "/locations/[^/]+",
+        pattern: "/(en|de|es|fr|pt|it|ja|pl)/locations/[^/]+",
         metaDataEndpoint: "https://xxgx-bhd0-k4hs.f2.xano.io/api:JyhvLhrj/location/metadata/{id}"
       }
       // Add more patterns and their metadata endpoints as needed
